bfdcd1985a64823413861f650fc47b63630e3dc3
mamzy <sarkhosh9580@gamil.com>
2025-10-09 19:46:49 -0700

    add a dir_base template & roots static & media in django settings
    add template & static files
    
    using template tags and extends for template
    
    using dynamic urls

--- website/urls.py
+++ website/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from website.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+app_name = 'website'
 
 
 urlpatterns = [
-    path('home', index_view),
-    path('about', about_view),
-    path('content', content_view)
+    path('', index_view, name='index'),
+    path('about', about_view, name='about'),
+    path('contact', contact_view, name='contact')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

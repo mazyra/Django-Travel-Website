d5fb8945edec5ba59934b18ad5d103f4e3977350
mamzy <sarkhosh9580@gamil.com>
2025-10-15 12:31:33 -0700

    - created models for blog $ website app & admin.py customaiz
    - add base.html to templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)    # admin.site.register(Post, PostAdmin)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'counted_views', 'status', 'published_date', 'created_date')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    # fields = ('title',)
+    # ordering = ['-created_date']
+
+
+

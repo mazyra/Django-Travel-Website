6168cbee9d98cb1e0ea3edad0def50ab3376381b
mamzy <sarkhosh9580@gamil.com>
2025-10-24 17:18:19 -0700

    - created category table & add to post table (ManyToMany)
    - rigester category to admin panel & use to blog template

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 @admin.register(Post)    # admin.site.register(Post, PostAdmin)
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-empty-'
-    list_display = ('title', 'counted_views', 'status', 'published_date', 'created_date')
-    list_filter = ('status',)
+    list_display = ('title', 'author','counted_views', 'status', 'published_date', 'created_date')
+    list_filter = ('status','author')
     search_fields = ['title', 'content']
     # fields = ('title',)
     # ordering = ['-created_date']
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
 
 
--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
-    #category 
+    category = models.ManyToManyField(Category)
     counted_views = models.PositiveIntegerField(default=0)
     #tag
     status = models.BooleanField(default=False)

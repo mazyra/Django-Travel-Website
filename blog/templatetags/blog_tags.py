2f9fee4e1f2fc5d3088d4d4d8bd083afb524e37c
mamzy <sarkhosh9580@gamil.com>
2025-11-06 13:16:08 -0800

    - show category in blog home with templatetags
    - filter cat_name & author_username with on function in views

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -7,3 +7,13 @@ register = template.Library()
 def latespost(arg=3):
     posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {'posts' : posts}
+
+@register.inclusion_tag('blog/blog-categories-post.html')
+def postcategori():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+
+    return {'categories' : cat_dict} 
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,12 @@ from django.http import HttpResponse
 from .models import Post
 
 
-def blog_view(request):
+def blog_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
+    if kwargs.get('cat_name'):
+        posts = posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('author_username'):
+        posts = posts.filter(author__username=kwargs['author_username'])
     context = {'posts' : posts}
     return render(request, 'blog/blog-home.html', context)
 
@@ -13,3 +17,8 @@ def blog_single(request, pid):
     post = get_object_or_404(post, pk=pid)
     context = {'post' : post}
     return render(request, 'blog/blog-single.html', context)
+
+# def blog_category(request, cat_name):
+#     posts = Post.objects.filter(status=1,category__name=cat_name)
+#     context = {'posts' : posts}
+#     return render(request, 'blog/blog-home.html', context)

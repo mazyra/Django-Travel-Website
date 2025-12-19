7884990082099044f0ed0a6d89403e2fcc53e5ce
mamzy <sarkhosh9580@gamil.com>
2025-12-19 21:11:01 +0330

    write logout function with login_required decorator

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from accounts.views import login_view, signup_view
+from accounts.views import login_view, logout_view, signup_view
 
 app_name = 'accounts'
 
 urlpatterns = [
     #login
-    path('login', login_view, name='login'),
+    path('login/', login_view, name='login'),
     #logout
-    #####
+    path('logout/', logout_view, name='logout'),
     #register
-    path('signup', signup_view, name='signup'),
+    path('signup/', signup_view, name='signup'),
 ]
